d828bdd148d5a67d64942d4bf9f281b32b23d101
Justin Gomez <programjustin@gmail.com>
2017-10-13 11:14:51 -0700

    convert callback to a promise

--- schema.js
+++ schema.js
@@ -1,8 +1,9 @@
 // node-fetch module fetches data from restAPI
 const fetch = require('node-fetch')
-
+// convert call back to a promise
+const util = require('util')
 // module xml2js
-require('xml2js').parseString;
+util.promisify(require('xml2js').parseString)
 
 // xml response
 const x = fetch(
